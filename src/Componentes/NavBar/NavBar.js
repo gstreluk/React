3865f31c57e60262585440f6e443acc9204ad337
gstreluk <guillestreluk@gmail.com>
2022-10-04 21:27:04 -0300

    agrego css

--- src/Componentes/NavBar/NavBar.js
+++ src/Componentes/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "./NavBar.css"
-import logo from "../../Assets/logo.jpg"
+import logo from "../../Assets/logo.png"
 import CartWidget from '../CartWidget/CartWidget'
 
 
